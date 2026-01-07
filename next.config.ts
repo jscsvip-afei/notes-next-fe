c20207317e1c99bd88d242289d5f3a0f0bac1dab
afei <lilong2576@hanweb.com>
2026-01-07 10:36:08 +0800

    chore: 删除不再使用的GitHub Actions工作流文件, github page不支持动态路由改动太多,采用edgeone平台部署,不进行静态部署

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: process.env.PAGES_BASE_PATH,
-  images: {
-    unoptimized: true, // GitHub Pages 不支持 Next.js 图片优化
-  },
+  /* config options here */
 };
 
 export default nextConfig;
